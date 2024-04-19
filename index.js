2e5459b7834c99fe974bf4056a1b4ef5d1f451a7
zachary oliver <zspoliver@gmail.com>
2024-04-19 12:03:50 -0400

    adds index.js

--- index.js
+++ index.js
@@ -2,33 +2,62 @@ function displayStadiums() {
     fetch("http://localhost:3000/stadiums")
     .then((resp) => resp.json())
     .then((data) => {
-        const imgContainer = document.querySelector('#stadium-image-container')
+        const imgContainer = document.querySelector('#stadium-image-container');
 
         data.forEach((stadiumObj) => {
-            const card = document.createElement('div')
-            card.classList.add('stadium-card')
+            const card = document.createElement('div');
+            card.classList.add('stadium-card');
 
-            const name = document.createElement('h3')
-            name.textContent = stadiumObj.name
-            card.appendChild(name)
+            const name = document.createElement('h3');
+            name.textContent = stadiumObj.name;
+            card.appendChild(name);
 
-            const open = document.createElement('h3')
-            open.textContent = stadiumObj.opened
-            card.appendChild(open)
+            const open = document.createElement('h3');
+            open.textContent = stadiumObj.opened;
+            card.appendChild(open);
 
-            const team = document.createElement('h3')
-            team.textContent = stadiumObj.team
-            card.appendChild(team)
+            const team = document.createElement('h3');
+            team.textContent = stadiumObj.team;
+            card.appendChild(team);
             
-            const img = document.createElement('img')
-            img.src = stadiumObj.image
-            img.setAttribute('id', 'images')
-            card.appendChild(img)
+            const img = document.createElement('img');
+            img.src = stadiumObj.image;
+            img.setAttribute('id', 'images');
+            card.appendChild(img);
 
-            imgContainer.appendChild(card)
-        })
-    })
-}
+            imgContainer.appendChild(card);
+        });
+    });
+};
+
+
+//* code that I tried to use to consolodate my code more. Reason for not using was because I was getting  a different putcome than I had wanted.*//
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     const teams = ['#steelers', '#rams', '#raiders', '#falcons', '#colts', '#cowboys', '#packers', '#lions', '#dolphins', '#chiefs'];
+
+//     function changeBgColor(e) {
+//         const newBackgroundColor = e.target;
+//         newBackgroundColor.style.backgroundColor = 'orange';
+//     }
+
+//     function revertBgColor(e) {
+//         const newBackgroundColor = e.target;
+//         newBackgroundColor.style.backgroundColor = '';
+//     }
+
+//     function alertMe(e) {
+//         const newBackgroundColor = e.target;
+//         newBackgroundColor.style.backgroundColor = 'lightgrey';
+//     }
+
+//     teams.forEach(team => {
+//         const newBackgroundColor = document.querySelector('#listed-stadiums');
+//         newBackgroundColor.addEventListener("mouseover", changeBgColor);
+//         newBackgroundColor.addEventListener("mouseout", revertBgColor);
+//         newBackgroundColor.addEventListener('click', alertMe);
+//     });
+// });
 
 document.addEventListener("DOMContentLoaded", function () {
     const newBackgroundColor = document.querySelector('#steelers');
